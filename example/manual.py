7ffa35182e0907769a0e4f38e29db473032522b4
Charlie Didear <topicscharlie@gmail.com>
2018-10-20 14:08:31 -0500

    Update example/manual.py

--- example/manual.py
+++ example/manual.py
@@ -51,10 +51,10 @@ while True:
 		bw.speed = backward_speed
 		lastCommand = timer()
 	elif uinput=='d':
-		fw.turn(1)
+		fw.turn(0)
 		lastCommand = timer()
 	elif uinput=='a':
-		fw.turn(1)
+		fw.turn(180)
 		lastCommand = timer()
 
 	else:
