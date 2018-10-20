4dcd8408a50ca584cf0067049c5438b203a84615
Charlie Didear <topicscharlie@gmail.com>
2018-10-20 12:45:45 -0500

    created manual.py
    Manual.py will be manual control of PiCar.

---
+++ example/manual.py
@@ -0,0 +1,31 @@
+from picar import front_wheels
+from picar import back_wheels
+import time
+import picar
+import random
+
+picar.setup()
+
+fw = front_wheels.Front_Wheels(db='config')
+bw = back_wheels.Back_Wheels(db='config')
+fw.turning_max = 45
+
+forward_speed = 70
+backward_speed = 70
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+while True:
+	uinput =getch();
