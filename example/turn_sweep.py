abd269fd3d753ef7aa2685fbce58635e4da80239
Charlie Didear <topicscharlie@gmail.com>
2018-10-20 14:36:33 -0500

    made turn_sweep.py

---
+++ example/turn_sweep.py
@@ -0,0 +1,22 @@
+#Program written by Charlie Didear.
+#picar libraries were made by Dream at SunFounder.
+from picar import front_wheels
+from picar import back_wheels
+from timeit import default_timer as timer
+import time
+import picar
+import random
+
+picar.setup()
+
+fw = front_wheels.Front_Wheels(db='config')
+bw = back_wheels.Back_Wheels(db='config')
+fw.turning_max = 45
+
+forward_speed = 70
+backward_speed = 70
+angle = 0
+while True:
+	for x in range (181):
+		print "angle: ()".format(angle)
+		angle = x
